d54166b16951656e7a41ef61a8e9c0dba9d9c109
Andrew Martin <am202@regenstrief.org>
2015-09-02 13:48:43 -0400

    CORE-2370: Improve CsvConverter flexibility

--- src/main/java/org/regenstrief/ohie/CsvConverter.java
+++ src/main/java/org/regenstrief/ohie/CsvConverter.java
@@ -16,7 +16,11 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 
 import org.regenstrief.hl7.convert.Escaper;
+import org.regenstrief.hl7.datatype.NM;
+import org.regenstrief.hl7.datatype.XTN;
 import org.regenstrief.io.IoUtil;
+import org.regenstrief.util.Dates;
+import org.regenstrief.util.StopWatch;
 import org.regenstrief.util.Util;
 
 /**
@@ -35,7 +39,9 @@ public class CsvConverter {
     
     private final static String HL7_NAME_MIDDLE ="^";
     
-    private final static String HL7_DOB ="^^^^L||";
+    private final static String HL7_MOTHERS_MAIDEN_NAME = "^^^^L|";
+    
+    private final static String HL7_DOB ="|";
     
     private final static String HL7_GENDER ="|";
     
@@ -58,9 +64,6 @@ public class CsvConverter {
     private final static String HL7_END = "\n" +
                     "PV1||I\n\n";
     
-    // person_id,last,first,middle,dob,gender,street,city,state,zip,area_code,phone_number,ssn,global_person_id
-    // Number,Gender,GivenName,MiddleInitial,Surname,StreetAddress,City,State,ZipCode,Country,EmailAddress,Username,Password,TelephoneNumber,MothersMaiden,Birthday,CCType,CCNumber,CVV2,CCExpires,NationalID,UPS,Occupation,Company,Vehicle,Domain,BloodType,Pounds,Kilograms,FeetInches,Centimeters,GUID,Latitude,Longitude
-    
     private static int IND_MRN = -1;
     
     private static int IND_NAME_LAST = -1;
@@ -68,6 +71,8 @@ public class CsvConverter {
     private static int IND_NAME_FIRST = -1;
     
     private static int IND_NAME_MIDDLE = -1;
+    
+    private static int IND_MOTHERS_MAIDEN_NAME = -1;
     
     private static int IND_DOB = -1;
     
@@ -80,6 +85,8 @@ public class CsvConverter {
     private static int IND_STATE = -1;
     
     private static int IND_ZIP = -1;
+    
+    private static int IND_FULL_PHONE = -1;
     
     private static int IND_AREA_CODE = -1;
     
@@ -104,6 +111,8 @@ public class CsvConverter {
         final String format = args[1];
         if ("1".equals(format)) {
             initIndices1();
+        } else if ("2".equals(format)) {
+            initIndices2();
         } else {
             throw new IllegalArgumentException("Unrecognized format: " + format);
         }
@@ -122,23 +131,17 @@ public class CsvConverter {
                 write(HL7_NAME_LAST, IND_NAME_LAST);
                 write(HL7_NAME_FIRST, IND_NAME_FIRST);
                 write(HL7_NAME_MIDDLE, IND_NAME_MIDDLE);
-                write(HL7_DOB, IND_DOB);
+                write(HL7_MOTHERS_MAIDEN_NAME, IND_MOTHERS_MAIDEN_NAME);
+                write(HL7_DOB, formatDate(get(IND_DOB)));
                 write(HL7_GENDER, formatGender(get(IND_GENDER)));
                 write(HL7_STREET, IND_STREET);
                 write(HL7_CITY, IND_CITY);
                 write(HL7_STATE, IND_STATE);
                 write(HL7_ZIP, IND_ZIP);
-                final String areaCode = get(IND_AREA_CODE), phone = get(IND_PHONE), fullPhone;
-                if (Util.isEmpty(phone)) {
-                    fullPhone = "";
-                } else if (Util.isEmpty(areaCode) || areaCode.equals("000")) {
-                    fullPhone = phone;
-                } else {
-                    fullPhone = "(" + areaCode + ")" + phone;
-                }
-                write(HL7_FULL_PHONE, fullPhone);
-                write(HL7_AREA_CODE, areaCode);
-                write(HL7_PHONE, Util.remove(phone, '-'));
+                final XTN xtn = getXtn();
+                write(HL7_FULL_PHONE, xtn.getNumber());
+                write(HL7_AREA_CODE, NM.getValue(xtn.getAreaCityCode()));
+                write(HL7_PHONE, NM.getValue(xtn.getPhoneNumber()));
                 write(HL7_SSN, IND_SSN);
                 out.write(HL7_END);
                 out.flush();
@@ -169,7 +172,52 @@ public class CsvConverter {
         return Util.isValued(gender) ? gender.substring(0, 1).toUpperCase() : null;
     }
     
+    private final static String formatDate(final String date) {
+        if (Util.isAllDigits(date) && (date.startsWith("19") || date.startsWith("20"))) {
+            return date;
+        }
+        return StopWatch.toHL7String(Dates.parseCalendar(date));
+    }
+    
+    private final static XTN getXtn() {
+        final XTN xtn = new XTN(null);
+        String fullPhone = get(IND_FULL_PHONE), areaCode = get(IND_AREA_CODE), phone = get(IND_PHONE);
+        final int fullSize = Util.length(fullPhone);
+        if ((fullSize >= 7) && Util.isEmpty(phone)) {
+            final String identifier = fullPhone.substring(fullSize - 4);
+            final int end = fullSize - ((fullPhone.charAt(fullSize - 5) == '-') ? 5 : 4); 
+            final String exchange = fullPhone.substring(end - 3, end);
+            phone = identifier + exchange;
+            if (fullSize >= 10) {
+                final char char0 = fullPhone.charAt(0);
+                final int start;
+                if (char0 == '(') {
+                    start = 1;
+                } else if (fullPhone.charAt(1) == '-') {
+                    start = 2;
+                } else if (char0 == 1) {
+                    start = 1;
+                } else {
+                    start = 0;
+                }
+                areaCode = fullPhone.substring(start, start + 3);
+            }
+        } else if (Util.isValued(phone) && Util.isEmpty(fullPhone)) {
+            if (Util.isEmpty(areaCode) || areaCode.equals("000")) {
+                fullPhone = phone;
+            } else {
+                fullPhone = "(" + areaCode + ")" + phone;
+            }
+        }
+        phone = Util.remove(phone, '-');
+        xtn.setNumber(fullPhone);
+        xtn.setAreaCityCode(NM.create(null, areaCode));
+        xtn.setPhoneNumber(NM.create(null, phone));
+        return xtn;
+    }
+    
     private final static void initIndices1() {
+        // person_id,last,first,middle,dob,gender,street,city,state,zip,area_code,phone_number,ssn,global_person_id
         IND_MRN = 0;
         IND_NAME_LAST = 1;
         IND_NAME_FIRST = 2;
@@ -183,5 +231,26 @@ public class CsvConverter {
         IND_AREA_CODE = 10;
         IND_PHONE = 11;
         IND_SSN = 12;
+    }
+    
+    private final static void initIndices2() {
+        // Number,Gender,GivenName,MiddleInitial,Surname,StreetAddress,City,State,ZipCode,Country,EmailAddress,Username,Password,TelephoneNumber,MothersMaiden,Birthday,
+        // CCType,CCNumber,CVV2,CCExpires,NationalID,UPS,Occupation,Company,Vehicle,Domain,BloodType,Pounds,Kilograms,FeetInches,Centimeters,GUID,Latitude,Longitude
+        IND_MRN = 0;
+        IND_GENDER = 1;
+        IND_NAME_FIRST = 2;
+        IND_NAME_MIDDLE = 3;
+        IND_NAME_LAST = 4;
+        IND_STREET = 5;
+        IND_CITY = 6;
+        IND_STATE = 7;
+        IND_ZIP = 8;
+        //IND_COUNTRY = 9;
+        //IND_EMAIL = 10;
+        //IND_USERNAME = 11;
+        //IND_PASSWORD = 12;
+        IND_FULL_PHONE = 13;
+        IND_MOTHERS_MAIDEN_NAME = 14;
+        IND_DOB = 15;
     }
 }
